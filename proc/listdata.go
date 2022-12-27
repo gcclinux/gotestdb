d1d4437a0779d229d61d967a9bfad3eaa80a33a3
Ricardo Wagemaker <java@wagemaker.co.uk>
2022-12-27 16:17:57 +0000

    Tue, Dec 27, 2022  4:17:57 PM

--- proc/listdata.go
+++ proc/listdata.go
@@ -15,6 +15,9 @@ func ListData() {
 	rockslice = append(rockslice, "Brandon", "Flowers", "41")
 
 	fmt.Println("List available data to upload")
+	fmt.Println()
+
+	fmt.Println("------------METHOD I------------")
 	n := 1
 	for i := 0; i <= len(rockslice)-1; i++ {
 		fmt.Print(rockslice[i], " ")
@@ -24,6 +27,18 @@ func ListData() {
 		}
 		n++
 	}
-	fmt.Println("-------------------------------------")
+	// Method 2 allows you to insert the data from line 36
+	fmt.Println("------------METHOD II-----------")
+	var readyslice []string
+	r := 1
+	for i := 0; i <= len(rockslice)-1; i++ {
+		readyslice = append(readyslice, rockslice[i])
+		if r == 3 {
+			fmt.Println("--> ", readyslice[0], readyslice[1], readyslice[2])
+			r = 0
+			readyslice = nil
+		}
+		r++
+	}
 
 }
